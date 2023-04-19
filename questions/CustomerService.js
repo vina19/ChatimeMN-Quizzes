eebfe6558e688048fd505a0515026584a9ab3850
Vina Kurniasari <sj9962nf@go.minneapolis.edu>
2023-04-19 10:02:17 -0500

    Added questions 1-3 for customer service

--- questions/CustomerService.js
+++ questions/CustomerService.js
@@ -1,68 +1,36 @@
 let CustomerServiceQuestions = [
     {
-        "question": "d",
-        "option1": "",
-        "option2": "",
-        "option3": "",
-        "option4": "",
-        "answer": 4
+        "question": "What is the price difference to upgrade to a large size drink?",
+        "option1": "$0.50",
+        "option2": "$0.75",
+        "option3": "$0.85",
+        "option4": "$0.25",
+        "answer": 2
     },
     {
-        "question": "",
-        "option1": "",
-        "option2": "",
-        "option3": "",
-        "option4": "",
-        "answer": 4
-    },
-    {
-        "question": "",
-        "option1": "",
-        "option2": "",
-        "option3": "",
-        "option4": "",
+        "question": "Do we offer milk substitutes?",
+        "option1": "We do offer almond milk sub for tea lattes.",
+        "option2": "We don't. We only offer whole milk.",
+        "option3": "We do offer soy milk sub for our lattes.",
+        "option4": "We don't. We only offer 2% milk.",
         "answer": 1
+    },
+    {
+        "question": "What are our caffeine free drink options?",
+        "option1": "",
+        "option2": "",
+        "option3": "All of our smoothies, breezes, creme brulee, water base fruit tea, taro milk tea, and horchata milk tea.",
+        "option4": "",
+        "answer": 3
     },
       
     {
-        "question": "",
+        "question": "Which of our toppings are vegan?",
         "option1": "",
         "option2": "",
         "option3": "",
-        "option4": "",
-        "answer": 2
-    },
-    {
-        "question": "",
-        "option1": "",
-        "option2": "",
-        "option3": "",
-        "option4": "",
-        "answer": 1
-    },
-    {
-        "question": "",
-        "option1": "",
-        "option2": "",
-        "option3": "",
-        "option4": "",
-        "answer": 3
-    },
-    {
-        "question": "",
-        "option1": "",
-        "option2": "",
-        "option3": "",
-        "option4": "",
+        "option4": "Pearls, ",
         "answer": 4
-    },
-    {
-        "question": "",
-        "option1": "",
-        "option2": "",
-        "option3": "",
-        "option4": "",
-        "answer": 3
     },
     {
         "question": "",
@@ -87,6 +55,38 @@ let CustomerServiceQuestions = [
         "option3": "",
         "option4": "",
         "answer": 4
+    },
+    {
+        "question": "",
+        "option1": "",
+        "option2": "",
+        "option3": "",
+        "option4": "",
+        "answer": 3
+    },
+    {
+        "question": "",
+        "option1": "",
+        "option2": "",
+        "option3": "",
+        "option4": "",
+        "answer": 2
+    },
+    {
+        "question": "",
+        "option1": "",
+        "option2": "",
+        "option3": "",
+        "option4": "",
+        "answer": 2
+    },
+    {
+        "question": "",
+        "option1": "",
+        "option2": "",
+        "option3": "",
+        "option4": "",
+        "answer": 1
     },
     {
         "question": "",
