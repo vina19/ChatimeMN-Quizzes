fb55f9ba21146eb23dd004193c05c90ea1ecd9c9
Vina Kurniasari <sj9962nf@go.minneapolis.edu>
2024-02-29 23:40:58 -0600

    Change the questions

--- questions/CremeBruleeTop.js
+++ questions/CremeBruleeTop.js
@@ -1,99 +1,18 @@
 let CremeBruleeQuestions = [
     {
-        "question": "What is the recipe for Regular size with topping Ube Latte Creme Brulee?",
-        "option1": "Smear 2 ube paste inside around the cup, 50% ice, 1 sugar, half line whole milk, and up to 500 line creme brulee.",
-        "option2": "Smear 1.5 ube paste inside around the cup, 50% ice, 1 sugar, half line whole milk, and up to 500 line creme brulee.",
-        "option3": "Smear 2 ube paste inside around the cup, 50% ice, 0.8 sugar, half line whole milk, and up to 500 line creme brulee.",
-        "option4": "Smear 1.5 ube paste inside around the cup, 50% ice, 0.8 sugar, half line whole milk, and up to 500 line creme brulee.",
-        "answer": 4
-    },
-    {
-        "question": "What is the recipe for Regular size with topping Mango Jam Creme Brulee?",
-        "option1": "1 spoons of Mango Jam in serving cup, 60% ice, 0.4 sugar, 400 line of milk, and up to 500 line creme brulee.",
-        "option2": "2 spoons of Mango Jam in serving cup, 60% ice, 0.3 sugar, 400 line of milk, and up to 500 line creme brulee.",
-        "option3": "2 spoons of Mango Jam in serving cup, 60% ice, 0.4 sugar, 400 line of milk, and up to 500 line creme brulee.",
-        "option4": "1 spoons of Mango Jam in serving cup, 60% ice, 0.3 sugar, 400 line of milk, and up to 500 line creme brulee.",
-        "answer": 4
-    },
-    {
-        "question": "What is the recipe for Large size with topping Tiger Sugar Creme Brulee?",
-        "option1": "Drizzle brown sugar inside the rim of cup, 80% ice in the cup, 425 line of whole milk, and up to 500 line creme brulee.",
-        "option2": "Drizzle brown sugar inside the rim of cup, 80% ice in the cup, 500 line of whole milk, and up to 500 line creme brulee.",
-        "option3": "Drizzle brown sugar inside the rim of cup, 50% ice in the cup, 425 line of whole milk, and up to 500 line creme brulee.",
-        "option4": "Drizzle brown sugar inside the rim of cup, 50% ice in the cup, 500 line of whole milk, and up to 500 line creme brulee.",
-        "answer": 1
-    },
-      
-    {
-        "question": "What should we tell customer if they want almond milk in their creme brulee?",
-        "option1": "We don't need to tell customer anything.",
-        "option2": "Inform the customer that our creme brulee is made with dairy products (heavy cream and whole milk).",
-        "option3": "We can let customer know that they cannot subtitute the milk to almond milk.",
-        "option4": "Inform the customer that our creme brulee drinks can only be made with whole milk.",
+        "question": "Which of these statements is most true?",
+        "option1": "We offer all drinks on the menu iced or hot.",
+        "option2": "We offer most milk teas, fresh teas, and most tea lattes iced or hot.",
+        "option3": "We offer all milk teas iced or hot.",
+        "option4": "We offer fruit teas hot.",
         "answer": 2
     },
     {
-        "question": "What are the sugar options for Tiger Sugar Creme Brulee?",
-        "option1": "100% and 50% sugar options.",
-        "option2": "100% and 80% sugar options.",
-        "option3": "50% and no sugar options.",
-        "option4": "50% and 30% sugar options.",
-        "answer": 1
-    },
-    {
-        "question": "Can customer make the Creme Brulee drinks with no ice?",
-        "option1": "No, it is because Creme Brulee is our ice blended drinks.",
-        "option2": "No, we can only serve the Creme Brulee drinks hot.",
-        "option3": "No, creme brulee can only be served with less ice or regular amount of ice.",
-        "option4": "Yes, we can make the Creme Brulee drinks with no ice.",
-        "answer": 3
-    },
-    {
-        "question": "Do we offer Creme Brulee in hot?",
-        "option1": "Yes, we do offer Creme Brulee drinks hot.",
-        "option2": "Yes, we only served Creme Brulee in hot.",
-        "option3": "No, Creme Brulee is our ice blended drinks.",
-        "option4": "No, Creme Brulee can only be served over ice with less ice or regular ice.",
+        "question": "Describe the difference between matcha and jasmine green tea.",
+        "option1": "Matcha is more bitter than jasmine green tea.",
+        "option2": "Matcha is more floral than jasmine green tea.",
+        "option3": "Jasmine green tea is more earthy flavor than matcha.",
+        "option4": "Jasmine green tea is more floral flavor than matcha.",
         "answer": 4
-    },
-    {
-        "question": "Can customer subtitute the milk for the Creme Brulee drinks?",
-        "option1": "Yes, customer can have soy milk sub instead of whole milk.",
-        "option2": "Yes, customer can have coconut milk sub instead of whole milk.",
-        "option3": "Yes, customer can have almond milk sub instead of whole milk.",
-        "option4": "No, customer can only have the milk that comes with the drink.",
-        "answer": 3
-    },
-    {
-        "question": "What is best to describe what is Ube Latte Creme Brulee to the customer?",
-        "option1": "Sweet purple yam paste around the cup with whole milk and creme brulee on the top.",
-        "option2": "Taro paste around the cup with whole milk and creme brulee on the top.",
-        "option3": "Sweet purple yam drink with creme brulee on the top.",
-        "option4": "Taro milk tea with creme brulee on the top.",
-        "answer": 1
-    },
-    {
-        "question": "What is the recipe for Large size with topping Ube Latte Creme Brulee?",
-        "option1": "Smear 1.5 ube paste inside around the cup, 50% ice, 0.8 sugar, 400 line whole milk, and up to 500 line creme brulee.",
-        "option2": "Smear 2 ube paste inside around the cup, 60% ice, 1 sugar, 400 line whole milk, and up to 500 line creme brulee.",
-        "option3": "Smear 1.5 ube paste inside around the cup, 60% ice, 1 sugar, 400 line whole milk, and up to 500 line creme brulee.",
-        "option4": "Smear 2 ube paste inside around the cup, 50% ice, 0.8 sugar, 400 line whole milk, and up to 500 line creme brulee.",
-        "answer": 2
-    },
-    {
-        "question": "What is the recipe for Regular size with topping Tiger Sugar Creme Brulee?",
-        "option1": "Drizzle brown sugar inside the rim of cup, 60% ice in the cup, 500 line of whole milk, and up to 500 line creme brulee.",
-        "option2": "Drizzle brown sugar inside the rim of cup, 50% ice in the cup, 500 line of whole milk, and up to 500 line creme brulee.",
-        "option3": "Drizzle brown sugar inside the rim of cup, 50% ice in the cup, 425 line of whole milk, and up to 500 line creme brulee.",
-        "option4": "Drizzle brown sugar inside the rim of cup, 60% ice in the cup, 425 line of whole milk, and up to 500 line creme brulee.",
-        "answer": 4
-    },
-    {
-        "question": "What is the recipe for Large size with topping Mango Jam Creme Brulee?",
-        "option1": "1.5 spoons of Mango Jam in serving cup, 60% ice, 0.3 sugar, 400 line of milk, and up to 500 line creme brulee.",
-        "option2": "2 spoons of Mango Jam in serving cup, 60% ice, 0.4 sugar, 400 line of milk, and up to 500 line creme brulee.",
-        "option3": "1.5 spoons of Mango Jam in serving cup, 60% ice, 0.4 sugar, 400 line of milk, and up to 500 line creme brulee.",
-        "option4": "2 spoons of Mango Jam in serving cup, 60% ice, 0.3 sugar, 400 line of milk, and up to 500 line creme brulee.",
-        "answer": 3
     }
 ];
