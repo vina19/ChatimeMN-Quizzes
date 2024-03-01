8bbdaf4c93a949e89e005d7cf60fccf4a474a599
Vina Kurniasari <sj9962nf@go.minneapolis.edu>
2024-02-29 23:54:02 -0600

    Change the questions

--- questions/CremeBruleeTop.js
+++ questions/CremeBruleeTop.js
@@ -14,5 +14,74 @@ let CremeBruleeQuestions = [
         "option3": "Jasmine green tea is more earthy flavor than matcha.",
         "option4": "Jasmine green tea is more floral flavor than matcha.",
         "answer": 4
-    }
+    },
+    {
+        "question": "Which of these statements is most true?",
+        "option1": "Our Non- dairy creamer powder is vegan and lactose free.",
+        "option2": "Our Non-dairy creamer powder is lactose friendly, but is made with a 2% dairy extract (Whey protein)",
+        "option3": "Our Non-dairy creamer powder is lactose and vegan friendly.",
+        "option4": "Our Non-dairy creamer powder is lactose and vegan.",
+        "answer": 2 
+    },
+    {
+        "question": "What is our blue tea made with?", 
+        "option1": "Butterfly pea flowers", 
+        "option2": "Spirulina",
+        "option3": "Hibiscus", 
+        "option4": "Blue food coloring", 
+        "answer": 1
+    },
+5.	Which group of drinks are all caffeine free? 
+A. Dragon breeze, Hawaiian fruit tea, Horchata milk tea, and Mango frosty 
+B. Violet breeze, Tiger latte, Taro milk tea, and Strawberry frosty 
+C. Ube crème brulee, Matcha latte, Cookies and Cream milk tea, and Chai frosty 
+      Answer: B 
+6.	True or false? All our jellies are vegan. 
+Answer: True- all our jellies are made with agar-agar, a plant-based gelatin substitute. 
+7.	Select the answer that describes our signature milk tea best. 
+A.	Black tea with a non-dairy creamer powder. 
+B.	Black tea with a brown sugar flavor. 
+C.	A robust royal black tea with a proprietary non- dairy creamer blend. 
+Answer: C 
+8.	A customer returns with their drink, saying it doesn’t taste right. What is the best solution following Niko Niko policies? 
+A.	Refund the amount for their drink. 
+B.	After disposing of the original drink, offer to remake it for the customer or any other drink of their choosing. 
+C.	Tell them to get back in line and reorder a drink they do like. 
+Answer: B 
+9.	Do we offer milk alternatives? 
+A.	No, we do not offer milk substitutes. All our milk teas are made with a non-dairy creamer powder. 
+B.	Yes, any milk tea or tea latte can be made with an almond milk substitute. 
+C.	Yes, we offer an almond milk substitute for all our tea lattes. 
+Answer: C 
+10.	Select the answer that best describes the Hawaiian fruit tea. 
+A.	Passion fruit, Peach, Strawberry and a bit of black tea. Tastes like a fruit punch. 
+B.	Tart and tropical flavors. Made with water. 
+C.	Coconut, strawberry, and mango with a bit of black tea. Tastes like a fruit punch. 
+Answer: A 
+11.	What do we use as a sweetener? 
+A. Honey 
+B. Fructose syrup 
+C. High fructose corn syrup 
+Answer: B 
+12.	A customer has dietary restraints and cannot have sugar. Which of these drinks is a good option for them? 
+A.	Signature milk tea, 0% sugar level. 
+B.	Mango green tea, 0% sugar level.
+C.	Black tea latte, 0% sugar level. 
+Answer: C 
+13.	Do you need a shift lead to approve a refund for any reason? 
+A.	If you know how to, you can just process the refund. 
+B.	We do not offer refunds. 
+C.	Yes, a shift lead will need to approve any refunds made. 
+Answer: C 
+14.	Do we allow customers to mix flavors? 
+A.	Yes, a customer can pick as many different flavors as they’d like to add to any drink. 
+B.	No, we only offer drinks that are available on our menu. 
+C.	If you know the customer is a regular, you can let them mix any flavors they’d like. 
+Answer: B 
+15.	Select the answer that has our tea options listed in order of most caffeine to least caffeine. 
+A.	Signature, black tea, green tea. 
+B.	Black tea, signature, green tea. 
+C.	Green tea, black tea, signature. 
+Answer: A 
+
 ];
